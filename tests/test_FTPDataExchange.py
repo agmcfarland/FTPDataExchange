c914365fe08bf713d7388f9ed02040abd2fa3603
Alexander McFarland <alex.925@gmail.com>
2025-08-21 09:14:54 -0400

    Update test_FTPDataExchange.py

--- tests/test_FTPDataExchange.py
+++ tests/test_FTPDataExchange.py
@@ -9,9 +9,24 @@ from unittest.mock import patch, Mock
 from FTPDataExchange import FTPDataExchange
 
 
+def test_connect_to_remote_success(ftp_data_exchange):
+
+	
+	
+	# Mock a successful connection
+	with patch.object(ftp_data_exchange.ftp, 'login'):
+		ftp_data_exchange.connect_to_remote()
+
+	assert ftp_data_exchange.ftp.login.called
+	assert ftp_data_exchange.ftp.set_pasv.called
+	assert ftp_data_exchange.ftp.set_pasv.called == 'woo'
+
+
 @pytest.fixture
-def ftp_data_exchange():
+def mock_ftp_data_exchange():
 	# Create an instance of FTPDataExchange with mock FTP connection
+	mock_ftp = Mock()
+
 	with patch('ftplib.FTP') as MockFTP:
 		instance = FTPDataExchange('ftp_host', 'ftp_user', 'ftp_passwd')
 		instance.ftp = MockFTP.return_value
